247be5488eaebca5e086aa7bb6512d97543fcd75
emoji <2362819691@qq.com>
2021-12-11 04:31:49 +0800

    add network framework

---
+++ network/network.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description : 神经网络框架
+@Date        : 2021/12/11 04:23:47
+@Author      : ifish
+@version     : 1.0
+'''
+
+import numpy as np
+
+
+class NeuralNetWork:
+    def __init__(self, path = None) -> None:
+        pass
+
+    def batch_data(self):
+        pass
+
+    def train(self):
+        pass
+
+    def saveNetWork(self, path):
+        pass
+
+    def addLayer(self, layer):
+        pass
+    
+    def gradient(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def loss(self):
+        pass
+
+    def accuracy(self):
+        pass
